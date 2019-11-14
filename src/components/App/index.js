68f684a88a9711fd33b65d95b7f37bf79ea583ae
megant <emberarc@gmail.com>
2019-11-14 10:56:55 +0100

    Layout updated

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,28 +1,10 @@
-import React from 'react';
-import logo from '../../images/logo.svg';
-import Apple from "@clientBase/components/Apple"
-import './App.css';
+import React from 'react'
+import Layout from '../Layout'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Apple />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <Layout />
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/Layout/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from 'client-base/src/components/Header'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const Layout = () => {
+    return (
+        <div className="container-fluid">
+          <Header />
+        </div>
+    )
+}
+
+export default Layout
